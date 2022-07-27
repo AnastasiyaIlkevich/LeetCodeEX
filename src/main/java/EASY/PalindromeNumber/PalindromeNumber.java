581b0a0967bdc5b38357bd5f4ca03b0df8bc6fdb
AnastasiyaIlkevich <sirin199@yandex.ru>
2022-07-27 22:34:02 +0300

    Palindrome Number (#9)

--- src/main/java/EASY/PalindromeNumber/PalindromeNumber.java
+++ src/main/java/EASY/PalindromeNumber/PalindromeNumber.java
@@ -2,8 +2,25 @@ package EASY.PalindromeNumber;
 
 public class PalindromeNumber {
     public static void main(String[] args) {
-        String str = String.valueOf(121);
+        //1
+        int x = 121;
+        String str = String.valueOf(x);
         String str2 = new StringBuilder(str).reverse().toString();
         System.out.println(str.equals(str2));
+
+        //2
+        int xx = x;
+        int temp = 0;
+        boolean flag = false;
+        while (xx > 0) {
+
+            temp = (xx % 10) + (temp * 10);
+            xx = xx / 10;
+        }
+        if (x == temp) {
+            flag = true;
+        }
+        System.out.println(flag);
     }
 }
+
