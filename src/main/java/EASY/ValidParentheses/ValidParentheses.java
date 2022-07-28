1e0deda72cd6c110b186a7018768d589d519a037
AnastasiyaIlkevich <sirin199@yandex.ru>
2022-07-28 13:07:59 +0300

    Valid Parentheses (#20)

---
+++ src/main/java/EASY/ValidParentheses/ValidParentheses.java
@@ -0,0 +1,29 @@
+package EASY.ValidParentheses;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class ValidParentheses {
+    public static void main(String[] args) {
+        String s = "({[]}()())";
+        boolean b = false;
+
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == ')' && !stack.isEmpty() && stack.peek() == '(') {
+                stack.pop();
+            } else if (c == ']' && !stack.isEmpty() && stack.peek() == '[') {
+                stack.pop();
+            } else if (c == '}' && !stack.isEmpty() && stack.peek() == '{') {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+        b = stack.isEmpty();
+        System.out.println(b);
+    }
+
+
+}
